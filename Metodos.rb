e4c5bcb932c3e3021eca431ecaf91937e4a858ce
Miguel Andrey Lopez <miguelandrey_21@live.com>
2018-10-18 17:26:13 -0500

    metodos

---
+++ Metodos.rb
@@ -0,0 +1,29 @@
+# Metodos, es un conjunto de declaraciones para realizar una tarea en especifica.
+
+
+def hola #Los metodos se definen en minusculas.
+    puts "Hola q ase"
+end
+
+hola #Invocacion del metodo.
+
+#Parametros
+
+def sqr(x)
+    puts x*x
+end
+
+sqr(4)
+
+#Multiples parametros.
+
+def suma(a,b)
+    puts a + b
+end
+
+x=2
+y=4
+
+suma(x,y)
+
+#parametros predeterminados
