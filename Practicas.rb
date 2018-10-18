29190ce85b496e9be00ea214f3da388d0998b586
Miguel Andrey Lopez <miguelandrey_21@live.com>
2018-10-18 11:16:52 -0500

    se suben mas practicas

--- Practicas.rb
+++ Practicas.rb
@@ -29,3 +29,51 @@ end
     when (35..45)
         puts "Bien y thu"
     end    
+
+
+nombre = 'Miguel';
+apellido='Lopez';
+tipoTelefono={
+    tipo1:'Celular',
+    tipo2:'TelefonoFijo',
+};
+
+edades=[
+    [1,3],[4,15],[16,18]
+];
+
+persona ={
+    :Nombre =>nombre,
+    :Apellido =>apellido,
+    :Telefono =>tipoTelefono,
+    :Edad => edades,
+    :Estatura =>'',
+}
+persona[:Estatura] = {
+    pequeno:'1.0 a 1.50',
+    mediano:'1.51 a 1.70',
+    grande:'1.71 a 2.0'
+};
+
+
+persona.each do |item,valor| 
+        
+    telefono1 = persona[:Telefono];
+    edad1 = persona[:Edad];
+    estatura1 = persona[:Estatura]
+        if item == :Telefono
+            telefono1.each do |item1,valor1|
+                puts "#{valor1}"
+            end
+            edad1.each do |item1,valor1|
+                puts "#{item1} #{valor1}"
+            end
+            estatura1.each do |item1,valor1|
+                puts "#{valor1}"
+            end
+        break          
+        end    
+        puts "#{valor}"
+end
+
+
